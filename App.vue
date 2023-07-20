93d7f981f442d7c86be837d4ef0a1053dc7d38c2
waykom <waykom1@gmail.com>
2023-07-20 12:40:37 +0800

    init

---
+++ App.vue
@@ -0,0 +1,30 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+@font-face {
+  font-family: 'iconfont';
+  src: url('~@/static/iconfont/iconfont.woff2') format('woff2'),
+       url('~@/static/iconfont/iconfont.woff') format('woff'),
+       url('~@/static/iconfont/iconfont.ttf') format('truetype');
+}
+.iconfont {
+  font-family: "iconfont" !important;
+  font-size: 16px;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ pages/tabBar/queAndAns/queAndAns.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		这是问答
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
